0af0cf0d76860853a8e2d609e8d2c0909949e6aa
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-16 13:50:49 -0600

    Changed concatenation in output string to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,9 @@ const assertEqual = function(actual, expected) {
   //If coded correctly the messages will be the required ones as per the
   //assignment
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: ",actual, " === " ,expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else
-    console.log("❌❌❌ Assertion Failed: ", actual, " !=== ",expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !=== ${expected}`);
   
 };
 
