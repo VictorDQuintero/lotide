b4b4250d6106350fcc66e65f16b3eaaa1b11871d
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-17 12:44:34 -0600

    Add code to tail.js

--- tail.js
+++ tail.js
@@ -15,11 +15,49 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(arr) {
 
-  /* Take the array, loop the elements and add them to the new result array
-  tArr[i-1] then send it out*/
+  /* Take the array, loop the elements and add them to the new result array,
+  prints resulting array, then returns it*/
 
-  const tArr = [];
+  const tailArr = [];
 
-  for (let  i = 1; i < arr.length; i++){}
+  for (let  i = 1; i < arr.length; i++) {
+    
+    tailArr[i - 1] = arr[i];
+    
+  }
+  
+  console.log(tailArr);
 
-}
+  return tailArr;
+};
+
+// TEST CASES
+
+// TEST CASE 1
+
+const arr = [1,2,3,4]; // Defines array
+tail(arr); //Calls function and sends arr, expected result [2,3,4]
+
+// TEST CASE 2
+
+const emptyArr = []; // Defines empty array
+const oneArr = ["No"]; // Defines one element array
+
+tail(emptyArr); // Expected result = []
+tail(oneArr); // Expected result = []
+
+// TEST CASE 3
+
+// Checks that original array is not changed
+
+const carArr = ["Fender", "Chevy", "Ford"];
+tail(carArr);
+assertEqual(carArr.length, 3); // Expected result: Assertion Passed: 3 === 3
+
+// TEST CASE 4
+
+const resultArr = tail(["Metallica", "Megadeth", "Anthrax", "Slayer"]);
+assertEqual(resultArr.length, 3); // Checks that resultArr has 3 elements
+assertEqual(resultArr[0], "Megadeth"); // Expected result Assertion Passed
+assertEqual(resultArr[1], "Anthrax"); // Expected result Assertion Passed
+assertEqual(resultArr[2], "Slayer"); // Expected result Assertion Passed
