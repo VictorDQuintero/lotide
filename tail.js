8a919a4f12b5dda1b57d6e7f6e5c926d2f342ff5
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-17 12:51:07 -0600

    Implement AI's suggestion of slice method

--- tail.js
+++ tail.js
@@ -18,17 +18,17 @@ const tail = function(arr) {
   /* Take the array, loop the elements and add them to the new result array,
   prints resulting array, then returns it*/
 
-  const tailArr = [];
+  /* const tailArr = [];
 
   for (let  i = 1; i < arr.length; i++) {
     
     tailArr[i - 1] = arr[i];
     
-  }
+  }*/
   
-  console.log(tailArr);
+  console.log(arr.slice(1));
 
-  return tailArr;
+  return arr.slice(1);
 };
 
 // TEST CASES
