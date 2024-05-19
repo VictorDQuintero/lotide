8ec3f8d8095b813fb438a781bb7ed4a871bafcf1
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-18 20:59:56 -0600

    Add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,46 @@
+//  FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  // Will take the two parameters and compare them with the strict equality operator
+  //If they are strictly equal it will print Equal or Not Equal, as a test
+  //If coded correctly the messages will be the required ones as per the
+  //assignment
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const eqArrays = function(arr1, arr2) {
+/* Function takes in two arays and returns true or false, based on a perfect match */
+
+  // Function must compare various things: length of the arrays, then the element inside the arrays
+  //If those things are strictly equal, then it returns True, if not function returns false
+
+  // Do this by iterating the arrays, and compare the elements simultaneously
+
+  let arrLength;
+  
+  if (arr1.length === arr2.length) {
+    arrLength = arr1.length;
+    // Proceeds to compare elements
+    for (let i = 0; i < arrLength; i++) {
+
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+
+    } return true;
+            
+  } else return false;
+  
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["A", "B", "C"], ["A", "B", "C"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should PASS
