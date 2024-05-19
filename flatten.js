2a8f5ea8c3e7d30fbc67d4233dd04debb604eeed
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-19 10:02:13 -0600

    Add error message if function doesn't get an array as input

--- flatten.js
+++ flatten.js
@@ -35,31 +35,25 @@ const flatten = function (arr) {
   /* Function takes in a nested array, one level deep, and 'flattens' it by turning it into an unnested array. */
   /* This function will only handle one level of nesting. */
 
-  //Loop array
-  //Check if element of the array is an array or not
-  //If not an array push element into new array
-  //If it is an array, loop that array and push elements into new array
-
   let newArr = [];
 
   if (!Array.isArray(arr)) {
-    // Tests wether array sent is array at all
-    return;
+    // Tests whether array sent is array at all
+    throw new Error("Input is not an array");
   }
 
   for (let element of arr) {
-    // Loops array
     if (!Array.isArray(element)) {
       // If element of array is not an array pushes element into new array
       newArr.push(element);
     } else {
       for (let nestElement of element) {
-        // If element IS an array, the element is looped through
-        newArr.push(nestElement); // Each element of the array gets pushed into new array
+        // If element IS an array, the element is looped through and each element of the array gets pushed into new array
+        newArr.push(nestElement);
       }
     }
   }
-  return newArr; // Function returns new array
+  return newArr;
 };
 
 let array = [1, 2, [3, 4], 5, [6]];
