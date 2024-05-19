6c2f3cba5516e9d740763f75a35aa03176feb969
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-18 21:48:11 -0600

    Fix comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -35,10 +35,10 @@ const assertArraysEqual = function(arr1, arr2) {
     
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //=> should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); //=> should FAIL
 assertArraysEqual(["A", "B", "C"], ["A", "B", "C"]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); // => should FAIL
   
  
