db67b70623600f77a526e7495ba5ef70f2c0be4b
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-21 11:45:41 -0600

    Add eqArrays and assertArraysEqual to letterPositions.js, Deleted unnecessary comment in assertArraysEqual.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,35 @@
 // FUNCTION IMPLEMENTATION
 
+const eqArrays = function (arr1, arr2) {
+  /* Function takes in two arays and returns true or false, based on a perfect match */
+
+  // Function must compare various things: length of the arrays, then the element inside the arrays
+  //If those things are strictly equal, then it returns True, if not function returns false
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Proceeds to compare corresponding elements
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  /* Function takes in two arrays and prints whether they are equal or not */
+
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
 const letterPositions = function (sentence) {
   const results = {};
   // logic to update results here
