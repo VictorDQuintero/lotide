f070b68415591c68d3cab11e3d26497c20603c85
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-21 17:39:20 -0600

    Implement code that tests whether objects have same number of keys and when they do all the values in one object match the values of the other

--- eqObjects.js
+++ eqObjects.js
@@ -13,4 +13,37 @@ const assertEqual = function (actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {};
+const eqObjects = function (object1, object2) {
+  let isMatch = false;
+  //must stablish whether the objects have the same number of keys
+
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const [key1, value1] in Object.entries(object1)) {
+      for (const [key2, value2] in Object.entries(object2)) {
+        if (key1 === key2)
+          if (value1 === value2) {
+            isMatch = true;
+          } else {
+            isMatch = false;
+          }
+      }
+    }
+  } else {
+    throw new Error("Objects have unequal number of keys");
+  }
+
+  return isMatch;
+};
+
+// TEST CODE
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+
+const longSleeveShirtObject = {
+  size: "medium",
+  color: "red",
+  sleeveLength: "long",
+};
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
