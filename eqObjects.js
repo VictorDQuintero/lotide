3f35d9894a03d329cde29aa7af2e073040918a5a
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-21 16:50:32 -0600

    Add assertEqual function, add eqObjects function definition

---
+++ eqObjects.js
@@ -0,0 +1,16 @@
+//  FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  // Will take the two parameters and compare them with the strict equality operator
+  //If they are strictly equal it will print Equal or Not Equal, as a test
+  //If coded correctly the messages will be the required ones as per the
+  //assignment
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {};
