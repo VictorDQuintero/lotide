abecd2db93cd21a2023a79f348564db2791273a3
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-22 13:00:34 -0600

    Change arr1 and arr2 in assertArraysEquals' function definition to more descriptive actual and expected

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,13 +19,13 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function (actual, expected) {
   /* Function takes in two arrays and prints whether they are equal or not */
 
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: [${arr1}] !== [${arr2}]`);
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
