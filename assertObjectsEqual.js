6e8ec577e7767b53ddafb87ac63c1bef4d3e8346
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-22 13:20:51 -0600

    Add eqArrays function, and Add test cases

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,25 @@
 // FUNCTION IMPLEMENTATION
-const eqObjects = function(object1, object2) {
+const eqArrays = function (arr1, arr2) {
+  /* Function takes in two arays and returns true or false, based on a perfect match */
+
+  // Function must compare various things: length of the arrays, then the element inside the arrays
+  //If those things are strictly equal, then it returns True, if not function returns false
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Proceeds to compare corresponding elements
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function (object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
@@ -24,7 +44,7 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function (actual, expected) {
   // Will take to objects and console.log a PASS/FAIL message to the console
   const inspect = require("util").inspect; // Used to make assertions print out the actual objects and not [object Object]
 
@@ -41,7 +61,10 @@ const assertObjectsEqual = function(actual, expected) {
 
 const testObject1 = { A: 1, B: 2, C: 3 };
 const testObject2 = { a: 2, b: 4 };
+const testObject3 = { 1: [1, 2], 2: [2, 3] };
 
 assertObjectsEqual(testObject1, { A: 1, B: 2, C: 3 }); // PASS
 assertObjectsEqual(testObject2, { a: 2, b: 4 }); // PASS
 assertObjectsEqual(testObject1, testObject2); // FAIL
+assertObjectsEqual(testObject3, { 1: [1, 2], 2: [2, 3] }); // PASS
+assertObjectsEqual(testObject1, {}); //FAIL
