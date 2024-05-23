16b5a0a06b6b73ce147e0e0ca7cfde03ba129f55
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-23 14:31:41 -0600

    Add some test code

--- takeUntil.js
+++ takeUntil.js
@@ -4,3 +4,25 @@ const takeUntil = function (array, callback) {
   /* The function will return a "slice of the array with elements taken from the beggining", \
 until the callback returns a truthy value. The callback will only be provided the item in the array */
 };
+
+// TEST CODE
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
