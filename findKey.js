47a536cc6a8fee2efaf53469f1eae63047b28fed
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-23 15:20:30 -0600

    Add assertEqual function

---
+++ findKey.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function (actual, expected) {
+  // Will take the two parameters and compare them with the strict equality operator
+  //If they are strictly equal it will print Equal or Not Equal, as a test
+  //If coded correctly the messages will be the required ones as per the
+  //assignment
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
