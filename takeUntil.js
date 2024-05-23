ba399e81787b20f6749f43cfe1954b2c3bc9b88d
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-23 14:33:25 -0600

    Add assertArraysEqual and eqArrays

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,37 @@
 //  FUNCTION IMPLEMENTATION
 
+const eqArrays = function (arr1, arr2) {
+  /* Function takes in two arrays and returns true or false, based on a perfect match */
+
+  // Function must compare various things: length of the arrays, then the element inside the arrays
+  //If those things are strictly equal, then it returns True, if not function returns false
+
+  // Do this by iterating the arrays, and compare the elements simultaneously
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Proceeds to compare elements
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  /* Function takes in two arrays and prints whether they are equal or not */
+
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
 const takeUntil = function (array, callback) {
   /* The function will return a "slice of the array with elements taken from the beggining", \
 until the callback returns a truthy value. The callback will only be provided the item in the array */
