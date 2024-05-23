40b52f9ba7d6a8d5f016f211faf07073b96fb521
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-23 14:29:32 -0600

    Add function declaration for takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,6 @@
+//  FUNCTION IMPLEMENTATION
+
+const takeUntil = function (array, callback) {
+  /* The function will return a "slice of the array with elements taken from the beggining", \
+until the callback returns a truthy value. The callback will only be provided the item in the array */
+};
