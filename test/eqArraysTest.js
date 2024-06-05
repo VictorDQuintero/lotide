3d7a0f7001dfa345f0f83ef55a5e2e8e313c6085
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-05 13:40:21 -0600

    Clarify tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,8 @@
 const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(["A", "B", "C"], ["A", "B", "C"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => eqArrays should return true, assertEqual should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => eqArrays should return false, assertEqual should PASS
+assertEqual(eqArrays(["A", "B", "C"], ["A", "B", "C"]), true); // => eqArrays should return true, assertEqual should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => eqArrays should return false, assertEqual should PASS
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => eqArrays should return false, assertEqual should PASS
