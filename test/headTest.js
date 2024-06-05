29fd7c594c71483e71323ba0adc6b3caae288f36
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-05 12:24:55 -0600

    Add comments

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,8 @@
 const head = require("../head.js");
 const assertEqual = require("../assertEqual");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "undefined");
-assertEqual(head([]), undefined);
-assertEqual(head(["H", "e", "y"]), "h");
+assertEqual(head([5, 6, 7]), 5); // should pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // should pass
+assertEqual(head([]), "undefined"); // should fail
+assertEqual(head([]), undefined); // should pass
+assertEqual(head(["H", "e", "y"]), "h"); // Should fail
