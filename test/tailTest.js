049ba1c66c02bafa69cea0d681da1aaaf4b99611
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-05 14:38:04 -0600

    Implement mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,11 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-// TEST CASES
-
-const resultArr = tail(["Metallica", "Megadeth", "Anthrax", "Slayer"]);
-assertEqual(resultArr.length, 3); // Checks that resultArr has 3 elements
-assertEqual(resultArr[0], "Megadeth"); // Expected result Assertion Passed
-assertEqual(resultArr[1], "Anthrax"); // Expected result Assertion Passed
-assertEqual(resultArr[2], "Slayer"); // Expected result Assertion Passed
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(["5"]), []);
+  });
+});
