d72b3179be3f48cda43c4eb851803f812092b56f
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-05 14:32:47 -0600

    Add mocha and chai to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,4 @@
-const head = require("../head.js");
+/* const head = require("../head.js");
 const assertEqual = require("../assertEqual");
 
 assertEqual(head([5, 6, 7]), 5); // should pass
@@ -6,3 +6,16 @@ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // should pass
 assertEqual(head([]), "undefined"); // should fail
 assertEqual(head([]), undefined); // should pass
 assertEqual(head(["H", "e", "y"]), "h"); // Should fail
+ */
+
+const assert = require("chai").assert;
+const head = require("../head");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+});
