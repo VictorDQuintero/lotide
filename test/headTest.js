cb6269359626d999deaa61cee0cddaecec5482e8
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-05 14:40:35 -0600

    Implement mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,3 @@
-/* const head = require("../head.js");
-const assertEqual = require("../assertEqual");
-
-assertEqual(head([5, 6, 7]), 5); // should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // should pass
-assertEqual(head([]), "undefined"); // should fail
-assertEqual(head([]), undefined); // should pass
-assertEqual(head(["H", "e", "y"]), "h"); // Should fail
- */
-
 const assert = require("chai").assert;
 const head = require("../head");
 
