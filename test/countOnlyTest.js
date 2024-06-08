2b4c0ed965b0d0dccedb44417758a219206b9ee0
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 11:22:11 -0600

    Add tests in countOnlyTest.js

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,26 +1,77 @@
-// TEST CODE
-/* 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Aghouanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
+const assert = require("chai").assert;
+const countOnly = require("../countOnly");
 
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false,
+describe("#countOnly", () => {
+  it("returns 1 for Jason", () => {
+    assert.strictEqual(
+      countOnly(
+        [
+          "Karl",
+          "Salima",
+          "Aghouanna",
+          "Fang",
+          "Kavith",
+          "Jason",
+          "Salima",
+          "Fang",
+          "Joe",
+        ],
+        {
+          Jason: true,
+          Karima: true,
+          Fang: true,
+          Agouhanna: false,
+        }
+      )["Jason"],
+      1
+    );
+  });
+  it("returns undefined for Karima", () => {
+    assert.strictEqual(
+      countOnly(
+        [
+          "Karl",
+          "Salima",
+          "Aghouanna",
+          "Fang",
+          "Kavith",
+          "Jason",
+          "Salima",
+          "Fang",
+          "Joe",
+        ],
+        {
+          Jason: true,
+          Karima: true,
+          Fang: true,
+          Agouanna: false,
+        }
+      )["Karima"],
+      undefined
+    );
+  });
+  it("returns undefined for Aghouanna", () => {
+    assert.strictEqual(
+      countOnly(
+        [
+          "Karl",
+          "Salima",
+          "Aghouanna",
+          "Fang",
+          "Kavith",
+          "Jason",
+          "Salima",
+          "Fang",
+          "Joe",
+        ],
+        {
+          Jason: true,
+          Karima: true,
+          Fang: true,
+          Aghouanna: false,
+        }
+      )["Aghouanna"],
+      undefined
+    );
+  });
 });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Aghouanna"], undefined);
- */
