5abcd936d5e0b49f2202f9372849c7cdaf91b947
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:16:54 -0600

    Add test code

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,3 +1,36 @@
+const assert = require("chai").assert;
+const findKeyByValue = require("../findKeyByValue");
+
+describe("#findKeyByValue", () => {
+  it("returns A for({A: 1, B: 2, C: 3}, 1)", () => {
+    assert.strictEqual(findKeyByValue({ A: 1, B: 2, C: 3 }, 1), "A");
+  });
+  it("returns 'Not Found' for ({A: 1, B: 2, C: 3}, 4)", () => {
+    assert.strictEqual(findKeyByValue({ A: 1, B: 2, C: 3 }, 4), "Not Found");
+  });
+  // it("returns false for {A: '1'} equals {A: 1, B: 2}", () => {
+  //   assert.strictEqual(findKeyByValue({ A: 1 }, { A: 1, B: 2 }), false);
+  // });
+  // it("returns false for {A: '1'} equals {A: 1}", () => {
+  //   assert.strictEqual(findKeyByValue({ A: "1" }, { A: 1 }), false);
+  // });
+  // it("returns true for {} equals {}", () => {
+  //   assert.strictEqual(findKeyByValue({}, {}), true);
+  // });
+  // it("returns true for {1: [1, 2, 3]} equals {1: [1, 2, 3]}", () => {
+  //   assert.strictEqual(
+  //     findKeyByValue({ 1: [1, 2, 3] }, { 1: [1, 2, 3] }),
+  //     true
+  //   );
+  // });
+  // it("returns false for {1: ['1', 2, 3]} equals {1: [1, 2, 3]}", () => {
+  //   assert.strictEqual(
+  //     findKeyByValue({ 1: ["1", 2, 3] }, { 1: [1, 2, 3] }),
+  //     false
+  //   );
+  // });
+});
+
 /* 
 const bestTVShowsByGenre = {
   sciFi: "The Expanse",
