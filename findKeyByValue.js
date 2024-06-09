5550dca214f9099bb10cab2b5d37db089651eaf5
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:35:30 -0600

    Add code to make third test pass

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,8 @@
 // Function takes object and a variable, then it returns the first property key that matches with the value given.
 const findKeyByValue = function (object, inputValue) {
+  if (typeof object !== "object") {
+    return "No Object Provided";
+  }
   for (const key of Object.keys(object)) {
     if (object[key] === inputValue) {
       return key;
