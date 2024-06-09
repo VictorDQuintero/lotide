60966e2e9e2a4c27c20c31d01a64f2e325ddb0e3
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:43:59 -0600

    Add tests

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,22 +1,36 @@
+const assert = require("chai").assert;
+const findKey = require("../findKey");
+
+describe("#findKey", () => {
+  it("returns C for({ A: 2, B: 3, C: 5, D: 6 }, (x) => x >= 5)", () => {
+    assert.strictEqual(
+      findKey({ A: 2, B: 3, C: 5, D: 6 }, (x) => x >= 5),
+      "C"
+    );
+  });
+  it("returns 'noma'", () => {
+    assert.strictEqual(
+      findKey(
+        {
+          "Blue Hill": { stars: 1 },
+          Akaleri: { stars: 3 },
+          noma: { stars: 2 },
+          elBulli: { stars: 3 },
+          Ora: { stars: 2 },
+          Akelarre: { stars: 3 },
+        },
+        (x) => x.stars === 2
+      ),
+      "noma"
+    );
+  });
+  // it("returns 'undefined' for (1, 4)", () => {
+  //   assert.strictEqual(findKey(1, 4), "No Object Provided");
+  // });
+});
+
 /* 
-const letters = { A: 2, B: 3, C: 5, D: 6 };
-const result1 = findKey(letters, (x) => x >= 5);
 
-assertEqual(result1, "C");
-
-const result2 = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-); // => "noma"
-
-assertEqual(result2, "noma"); //PASS
 
 const testObject = {
   Jim: { age: 46, countryOfOrigin: "USA" },
