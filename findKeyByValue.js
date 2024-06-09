8fd9d6533b968b0c4ccdef9c670bd768c05ce12f
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:26:30 -0600

    Add code to make the second test pass

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,11 @@
 // Function takes object and a variable, then it returns the first property key that matches with the value given.
 const findKeyByValue = function (object, inputValue) {
-  for (const [key, value] of Object.entries(object)) {
-    if (value === inputValue) {
+  for (const key of Object.keys(object)) {
+    if (object[key] === inputValue) {
       return key;
     }
   }
+  return "Key Not Found";
 };
 
 module.exports = findKeyByValue;
