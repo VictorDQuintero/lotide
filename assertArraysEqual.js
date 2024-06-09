4aad33a9b5067407c927c709c48c0f7a15d8bf17
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 21:13:37 -0600

    Lint code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@
 const eqArrays = require("./eqArrays");
 
 // Function takes in two arrays and prints whether they are equal or not.
-const assertArraysEqual = function (actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
