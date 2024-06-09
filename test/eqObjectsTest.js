35bf9e92b873404b1347fcd772b8668244c62b06
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:09:40 -0600

    Delete comments

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -24,39 +24,3 @@ describe("#eqObjects", () => {
     assert.strictEqual(eqObjects({ 1: ["1", 2, 3] }, { 1: [1, 2, 3] }), false);
   });
 });
-
-/* 
-// TEST CODE
-// const empty = {};
-// console.log(eqObjects(empty, empty));
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
-
-const longSleeveShirtObject = {
-  size: "medium",
-  color: "red",
-  sleeveLength: "long",
-};
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-};
-assertEqual(
-  eqObjects(multiColorShirtObject, anotherMultiColorShirtObject),
-  true
-); // => true
-
-const longSleeveMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-  sleeveLength: "long",
-};
-assertEqual(
-  eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject),
-  false
-); // => false
- */
