49da894411b90313245bfbc033645f3c3b49e0c0
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:46:49 -0600

    Add code to makee third test pass

--- findKey.js
+++ findKey.js
@@ -1,6 +1,9 @@
 /* Function takes object and returns the first key for which the callback returns a truthy value.
   If no key is found, then it returns 'undefined'. */
 const findKey = function (object, callback) {
+  if (typeof object !== "object") {
+    return "No Object Provided";
+  }
   for (const [key, value] of Object.entries(object)) {
     if (callback(value)) {
       return key;
