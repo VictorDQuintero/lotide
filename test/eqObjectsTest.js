24aefd102fb79b5af0eddc2658187fce6b91ebd0
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:05:16 -0600

    Add tests

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,3 +1,30 @@
+const assert = require("chai").assert;
+const eqObjects = require("../eqObjects");
+
+describe("#eqObjects", () => {
+  it("returns true for {A: 1, B: 2, C: 3} equals {A: 1, B: 2, C: 3}", () => {
+    assert.strictEqual(
+      eqObjects({ A: 1, B: 2, C: 3 }, { A: 1, B: 2, C: 3 }),
+      true
+    );
+  });
+  it("returns false for {A: '1'} equals {A: 1, B: 2}", () => {
+    assert.strictEqual(eqObjects({ A: 1 }, { A: 1, B: 2 }), false);
+  });
+  it("returns false for {A: '1'} equals {A: 1}", () => {
+    assert.strictEqual(eqObjects({ A: "1" }, { A: 1 }), false);
+  });
+  it("returns true for {} equals {}", () => {
+    assert.strictEqual(eqObjects({}, {}), true);
+  });
+  it("returns true for {1: [1, 2, 3]} equals {1: [1, 2, 3]}", () => {
+    assert.strictEqual(eqObjects({ 1: [1, 2, 3] }, { 1: [1, 2, 3] }), true);
+  });
+  it("returns false for {1: ['1', 2, 3]} equals {1: [1, 2, 3]}", () => {
+    assert.strictEqual(eqObjects({ 1: ["1", 2, 3] }, { 1: [1, 2, 3] }), false);
+  });
+});
+
 /* 
 // TEST CODE
 // const empty = {};
