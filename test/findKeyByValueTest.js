4bfcf92cb730fd7630f808da33909fa139c72792
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:36:58 -0600

    Remove comments

--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -15,22 +15,3 @@ describe("#findKeyByValue", () => {
     assert.strictEqual(findKeyByValue(1, 4), "No Object Provided");
   });
 });
-
-/* 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-};
-
-const bandsByGenre = {
-  metal: "Metallica",
-  classicRock: "The Rolling Stones",
-  indie: "Metric",
-  punk: "The Clash",
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bandsByGenre, "Metric"), "indie");
- */
