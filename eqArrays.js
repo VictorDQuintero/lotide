7673d557d6aeb642bb2d085be6768757d5f2178e
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-08 20:08:20 -0600

    Add code to pass last test and comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,10 @@
 //  Function takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function (arr1, arr2) {
+  //if either input is not an array returns false
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+  //if arrays are different lengths (not equal by default), return false
   if (arr1.length !== arr2.length) {
     return false;
   }
